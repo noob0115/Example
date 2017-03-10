5bd1c79fda06c3a5a5710cd070544683a493470c
noob0115 <18710841568@163.com>
2017-03-10 18:37:49 +0800

    Create SortMain.cpp

---
+++ SortMain.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+#include <CPubilc.h>
+
+const int nSortBubble = 1;
+const int nSortInsert: = 2;
+using namespace std;
+
+int main(int argc,char *argv[])
+{
+    int nFlag = 0;
+    nFlag = atoi(argv{1});
+    
+    switch nFlag:
+        case nSortBubble: 
+        
+        case nSortInsert:
+        
+        default:
+            cout << "输入参数无效,请检查:" << endl;
+            cout << "1 冒泡排序" << endl;
+            cout << "2 插入排序" << endl;
+            return -1;
+    
+    return 0;
+}
